56baacf246795c3af228befc4cdc3b8260d5d044
Engineer-mostafa <Engineer-mostafa>
2021-04-05 01:13:11 +0200

    initial login

--- lib/view/auth/login_screen.dart
+++ lib/view/auth/login_screen.dart
@@ -1,11 +1,57 @@
+//Packages
 import 'package:flutter/material.dart';
+
+//widgets
+import '../../Constants.dart';
+
+//screens
+import '../widgets/custom_text.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final mediaQ = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text("hijhjg"),
+        elevation: 0.0,
+        title: Text("Login"),
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 20,
+          right: 20,
+          top: 50,
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CustomText(
+                  title: "Welcome",
+                  color: Colors.black,
+                  fontSize: mediaQ.width / 13,
+                ),
+                CustomText(
+                  title: "SignUp",
+                  color: primaryColor,
+                  fontSize: mediaQ.width / 22,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: mediaQ.height/80,
+            ),
+
+            CustomText(
+              title: "Sign in to Continue",
+              color: Colors.grey,
+              fontSize: mediaQ.width / 30,
+              alignment: Alignment.topLeft,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/view/widgets/custom_text.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  final title;
+  final color;
+  final fontSize;
+  final Alignment alignment;
+
+  const CustomText({Key key, this.title:"", this.color:Colors.black, this.fontSize:30, this.alignment:Alignment.center})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: alignment,
+      child: Text(
+        title,
+        style: TextStyle(color: color, fontSize: fontSize),
+      ),
+    );
+  }
+}
