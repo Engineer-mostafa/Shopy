8277112ef950c8d79de0f5b8de22cd65855926a5
Engineer-mostafa <Engineer-mostafa>
2021-04-06 20:47:28 +0200

    sync firebase (Andriod) + detectGesture to close keyboard

--- lib/view/auth/login_screen.dart
+++ lib/view/auth/login_screen.dart
@@ -11,8 +11,18 @@ import '../widgets/custom_text_form_field.dart';
 import '../widgets/custom_text.dart';
 
 class LoginScreen extends StatelessWidget {
+
+
+
+
   @override
   Widget build(BuildContext context) {
+    void closeKeyboard(){
+      FocusScopeNode currentFocus = FocusScope.of(context);
+      if (!currentFocus.hasPrimaryFocus) {
+        currentFocus.unfocus();
+      }
+    }
     final mediaQ = MediaQuery
         .of(context)
         .size;
@@ -28,105 +38,111 @@ class LoginScreen extends StatelessWidget {
           right: 20,
           top: 50,
         ),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: 
+        GestureDetector(
+          onTap: closeKeyboard,
+          child: SingleChildScrollView(
+            child: Column(
               children: [
-                CustomText(
-                  title: "Welcome",
-                  color: Colors.black,
-                  fontSize: mediaQ.width / 13,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CustomText(
+                      title: "Welcome",
+                      color: Colors.black,
+                      fontSize: mediaQ.width / 13,
+                    ),
+                    CustomText(
+                      title: "SignUp",
+                      color: primaryColor,
+                      fontSize: mediaQ.width / 22,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: mediaQ.height / 80,
                 ),
                 CustomText(
-                  title: "SignUp",
-                  color: primaryColor,
-                  fontSize: mediaQ.width / 22,
+                  title: "Sign in to Continue",
+                  color: Colors.grey,
+                  fontSize: mediaQ.width / 30,
+                  alignment: Alignment.topLeft,
+                ),
+                SizedBox(
+                  height: mediaQ.height / 20,
+                ),
+                CustomTextFormField(
+                  title: "Email",
+                  hintText: "mos@example.con",
+                  hintStyleColor: Colors.grey,
+                  fontSize: mediaQ.width / 25,
+                  onSave: (value) {},
+                  onValidator: (value) {},
+                ),
+                SizedBox(
+                  height: mediaQ.height / 20,
+                ),
+                CustomTextFormField(
+                  title: "Password",
+                  hintText: "***************",
+                  hintStyleColor: Colors.grey,
+                  fontSize: mediaQ.width / 25,
+                  onSave: (value) {},
+                  onValidator: (value) {},
+                ),
+                SizedBox(
+                  height: mediaQ.height / 40,
+                ),
+                CustomText(
+                  title: "forget password?",
+                  fontSize: mediaQ.width / 25,
+                  alignment: Alignment.topRight,
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  height: mediaQ.height / 30,
+                ),
+                CustomButton(
+                  padding: mediaQ.height / 70,
+                  fontSize: mediaQ.width / 20,
+                  borderRadius: mediaQ.width / 80,
+                  buttonColor: primaryColor,
+                  onpressed: (){},
+                ),
+                SizedBox(
+                  height: mediaQ.height / 70,
+                ),
+                CustomText(
+                  title: "-OR-",
+                fontSize: mediaQ.width/20,
+                  alignment: Alignment.center,
+                ),
+                SizedBox(
+                  height: mediaQ.height / 35,
+                ),
+                CustomButtonSocial(
+                    text: "Sign In With Google",
+                    imageName: 'assets/images/gg.png',
+                    onPress: (){},
+                  fontSize: mediaQ.width/26,
+                  borderRadius: mediaQ.width/50,
+                  width: mediaQ.width/4.5,
+                ),
+                SizedBox(
+                  height: mediaQ.height / 35,
+                ),
+                CustomButtonSocial(
+                  text: "Sign In With Facebook",
+                  imageName: 'assets/images/facebook.png',
+                  onPress: (){},
+                  fontSize: mediaQ.width/26,
+                  borderRadius: mediaQ.width/50,
+                  width: mediaQ.width/4.5,
+
                 ),
               ],
             ),
-            SizedBox(
-              height: mediaQ.height / 80,
-            ),
-            CustomText(
-              title: "Sign in to Continue",
-              color: Colors.grey,
-              fontSize: mediaQ.width / 30,
-              alignment: Alignment.topLeft,
-            ),
-            SizedBox(
-              height: mediaQ.height / 20,
-            ),
-            CustomTextFormField(
-              title: "Email",
-              hintText: "mos@example.con",
-              hintStyleColor: Colors.grey,
-              fontSize: mediaQ.width / 25,
-              onSave: (value) {},
-              onValidator: (value) {},
-            ),
-            SizedBox(
-              height: mediaQ.height / 20,
-            ),
-            CustomTextFormField(
-              title: "Password",
-              hintText: "***************",
-              hintStyleColor: Colors.grey,
-              fontSize: mediaQ.width / 25,
-              onSave: (value) {},
-              onValidator: (value) {},
-            ),
-            SizedBox(
-              height: mediaQ.height / 40,
-            ),
-            CustomText(
-              title: "forget password?",
-              fontSize: mediaQ.width / 25,
-              alignment: Alignment.topRight,
-              color: Colors.black,
-            ),
-            SizedBox(
-              height: mediaQ.height / 30,
-            ),
-            CustomButton(
-              padding: mediaQ.height / 70,
-              fontSize: mediaQ.width / 20,
-              borderRadius: mediaQ.width / 80,
-              buttonColor: primaryColor,
-              onpressed: (){},
-            ),
-            SizedBox(
-              height: mediaQ.height / 70,
-            ),
-            CustomText(
-              title: "-OR-",
-            fontSize: mediaQ.width/20,
-              alignment: Alignment.center,
-            ),
-            SizedBox(
-              height: mediaQ.height / 35,
-            ),
-            CustomButtonSocial(
-                text: "Sign In With Google",
-                imageName: 'assets/images/gg.png',
-                onPress: (){},
-              fontSize: mediaQ.width/26,
-              borderRadius: mediaQ.width/50,
-              width: mediaQ.width/4.5,
-            ),
-            SizedBox(
-              height: mediaQ.height / 35,
-            ),
-            CustomButtonSocial(
-              text: "Sign In With Facebook",
-              imageName: 'assets/images/facebook.png',
-              onPress: (){},
-              fontSize: mediaQ.width/26,
-              borderRadius: mediaQ.width/50,
-              width: mediaQ.width/4.5,
-
-            ),
-          ],
+          ),
         ),
       ),
     );
